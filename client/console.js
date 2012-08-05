d8d7e811b945d4ad5b044d414749fc095dc47677
jmptable <trueowen@verizon.net>
2012-08-05 04:43:11 -0700

    added more console logic

--- client/console.js
+++ client/console.js
@@ -1,18 +1,27 @@
-function Console(canvas) {
+function Console(scrn) {
 	this.buffer = "";
-	
+	this.limit = 64;
+	this.screen = scrn;
 }
 
 Console.prototype = {
-	input: function(msg) {
-		if(msg.charCodeAt(0)==13) {
-			this.process();
-		} else {
-			this.buffer += msg;
+	input: function(code) {
+		if(code.length<=this.limit) {
+			switch(code) {
+				case 13:	//ENTER
+					this.process();
+					break;
+				case 8:		//BACKSPACE
+					this.buffer = this.buffer.substring(0, length-1);
+					this.screen.dirty = false;
+					breakl
+				default:
+					this.process();
+					break;
+			}
 		}
 	},
 	
 	process: function() {
-		
 	}
 }
