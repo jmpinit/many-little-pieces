5730c0c9ac11ec1a348a502094c8fb8e4f42adda
Shelvacu L Vevevende <shelavacu@gmail.com>
2012-08-05 06:33:22 -0700

    server

--- server/pieceserver.js
+++ server/pieceserver.js
@@ -224,7 +224,7 @@ http.createServer(function (request, response) {
         }
         else {
 			content = 'test'
-            response.writeHead(200, { 'Content-Type': 'text/htmls' });
+            response.writeHead(200, { 'Content-Type': 'text/html' });
             response.end(content, 'utf-8');
         }
     });
