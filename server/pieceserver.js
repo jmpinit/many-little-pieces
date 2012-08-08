460ce2137cfdb9bbd1fb69e2986f31272013b2aa
jmptable <trueowen@verizon.net>
2012-08-07 17:25:03 -0700

    Clean house.

--- server/pieceserver.js
+++ server/pieceserver.js
@@ -1,95 +1,26 @@
 var Maple = require('../Maple/Maple');
 
-var db = require('mongojs').connect('pieces');
-var world = db.collection('world');
-var users = db.collection('users');
-var sessions = db.collection('session');
+var plasma = {
+	enabled: false,
+	time: 0,
+	heightmap: " .'`,^:+';~-_+<>i!lI?/|()1{}[]rcvunxzjftLCJUYXZO0Qoahkbdpqwm*WMB8&%$#@"
+}
 
-console.log("world initialized");
+var world = new Array();
 
-var Players = new Array();
+var players = new Array();
 
-function Player(x, y, connect) {
+function Player(x, y) {
 	this.x = x;
 	this.y = y;
-	this.connect = connect;	
+	this.inventory = "";
 }
 
 Player.prototype = {
-	moveUp: function(connect) {
-	var query = { "loc" : [this.x , this.y ] };
-		world.find(query, function(err, docs) {
-			if(docs.length != 0){
-			srv.broadcast(protocol.type.WORLD, [docs[0]], [connect]);
-			console.log("map sent");
-			} else {
-			console.log("Bad Input"+connect.id);
-			}
-			});
-
-	},
-	moveDown: function(connect) {
-	var query = { "loc" : [this.x  , this.y ] };
-	world.find(query, function(err, docs) {
-			if(docs.length != 0){
-			srv.broadcast(protocol.type.WORLD, [docs[0]], [connect]);
-			console.log("map sent");
-			} else {
-			console.log("Bad Input"+connect.id);
-			}
-			});
-   },
-	moveLeft: function(connect) {
-	var query = { "loc" : [this.x , this.y] };
-	world.find(query, function(err, docs) {
-			if(docs.length != 0){
-			srv.broadcast(protocol.type.WORLD, [docs[0]], connect);
-			console.log("map sent");
-			} else {
-			console.log("Bad Input"+connect.id);
-			}
-			});
 	
-	},
-	moveRight: function(connect) {
-	var query = { "loc" : [this.x , this.y] };
-	world.find(query, function(err, docs) {
-			if(docs.length != 0){
-			   srv.broadcast(protocol.type.WORLD, [docs[0]], [connect]);
-			   console.log("map sent");
-			} else {
-			console.log("Bad Input"+connect.id);
-			}
-			});
-	//this.renderLoc(query );
-	},
-	renderLoc: function( query ){
-		 console.log("In render Loc");
-			world.find(query, function(err, docs) {
-			if(docs.length != 0){
-			srv.broadcast(protocol.type.WORLD, [docs[0]], [this.connect]);
-			console.log("map sent");
-			} else {
-			console.log("Bad Input"+connect.id);
-			}
-			});
-	}	
 }
 
-function commandLogic(type, data ){
-	
-	switch(type) {
-	
-	case MOVE_RIGHT:
-		break;	
-	case MOVE_LEFT:
-		break;
-	case MOVE_UP:
-		break;
-	case MOVE_DOWN:
-	}
-}
-
+players[0] = new Player(0, 0);
 
 var PieceServer = Maple.Class(function(clientClass) {
 	Maple.Server(this, clientClass);
@@ -99,32 +30,43 @@ var PieceServer = Maple.Class(function(clientClass) {
 	started: function() {
 		console.log('Started');
 		
-		//drop the existing chunks
-		world.drop();
-
-		//create some chunks
-		for(var y=0; y<8; y++) {
-			for(var x=0; x<8; x++) {
-				var chunkform = { loc: [ ], data: "" };
-				chunkform.loc = [ x*world_info.SCREEN_W ,chunkform.loc.y = y*world_info.SCREEN_H] ;
-				
-				for(var i=0; i<world_info.SCREEN_H*world_info.SCREEN_W; i++) {
-					chunkform.data += "X";//world_info.characters[Math.floor(Math.random()*96+32];
+		//create the world
+		for(var y=0; y<64; y++) {
+			for(var x=0; x<64; x++) {
+				if(Math.random()*100>90) {
+					world[y*64+x] = String.fromCharCode(Math.floor(32+Math.random()*94));
+				} else {
+					world[y*64+x] = " ";
 				}
-				world.save(chunkform);
 			}
 		}
-		//create dummy player
-		users.save({
-			name: "nehal",
-			password: "password",
-			loc: [ 32,128 ]
-		});
+		
+		/*world.getPos = function(x, y) {
+			return world[y*512+x];
+		}*/
 	},
 
 	update: function(t, tick) {
-		//this.broadcast(5, ['Hello World']);
-		//console.log(this.getClients().length, 'client(s) connected', t, tick, this.getRandom());
+		enemies = new Array();
+		for(var i in players) {
+			enemies[enemies.length] = players[i];
+		}
+		this.broadcast(3, [JSON.stringify(enemies), JSON.stringify(world)]);
+		
+		if(plasma.enabled) {
+			for(var y=0; y<64; y++) {
+				for(var x=0; x<64; x++) {
+					var t = plasma.time;
+					var d = function(x1, y1, x2, y2) { return Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2)); }
+					var p1 = (Math.sin(x/(8*Math.cos(t/16)))+1);
+					var p2 = (Math.sin(d(12+8*Math.cos(t/8), 12+8*Math.sin(t/8), x, y)/32)+1);
+					var i = Math.floor(plasma.heightmap.length*((p1+p2)/4));
+					world[y*64+x] = plasma.heightmap.charAt(i);
+				}
+			}
+			
+			plasma.time += Math.PI/32;
+		}
 	},
 
 	stopped: function() {
@@ -133,83 +75,103 @@ var PieceServer = Maple.Class(function(clientClass) {
 
 	connected: function(client) {
 		console.log('Connected:', client.id);
+		players[client.id] = new Player(3, 3);
 	},
 
-message: function(client, type, tick, data) {
-				 console.log('ClientInfo:', client.id);
-				 console.log('Message:', data);
-				 switch(type) {
-						 case protocol.type.INIT:
-								 users.find(data.name, function(err, docs) {
-												 //if(docs[0].password==data.password) {
-												 if(docs.length>0) {
-												 var query = { "loc" : docs[0].loc};
-												 console.log(query);		
-												 world.find(query, function(err, docs) {
-														 console.log(docs[0]);
-														 srv.broadcast(protocol.INIT, [docs[0]], [client]);
-														 console.log("map sent");
-														 });
-												 //TODO check if we already have this player in list		
-												new_player = new Player(docs[0].loc[0],docs[0].loc[0],client); 
-												Players.push(new_player);		
-												console.log("NEw plyaer added "+ Players.length);
-												 
-												} 
-												 });
-								 break;
-						 case protocol.type.COMMAND:
+	message: function(client, type, tick, data) {
+		switch(type) {
+			case protocol.type.INIT:
+				
+				break;
+			case protocol.type.COMMAND:
+				switch(data[0].dir){
+					case "U": // W up
+						players[client.id].y--;
+						break;
+					case "R": // D right
+						players[client.id].x++;
+						break;
+					case "D" : // S down
+						players[client.id].y++;
+						break;
+					case "L" : // A Left
+						players[client.id].x--;
+						break;
+				}
+				
+				var tile = world[players[client.id].y*64+players[client.id].x];
+				if(tile!=" ") {
+					players[client.id].inventory += tile;
+					world[players[client.id].y*64+players[client.id].x] = " ";
+				}
+				
+				console.log("player "+client.id+" is now at ("+players[client.id].x+", "+players[client.id].y+")");
+				
+				break;
+			case protocol.type.CONSOLE:
+				console.log(data[0]);
+				switch(data[0]) {
+					case "PLASMA":
+						plasma.enabled = true;
+						break;
+					case "RAND":
+						for(var y=0; y<64; y++) {
+							for(var x=0; x<64; x++) {
+								if(Math.random()*100>90) {
+									world[y*64+x] = String.fromCharCode(Math.floor(32+Math.random()*94));
+								} else {
+									world[y*64+x] = " ";
+								}
+							}
+						}
+						break;
+					case "CLEAR":
+						for(var y=0; y<64; y++) {
+							for(var x=0; x<64; x++) {
+								world[y*64+x] = " ";
+							}
+						}
+						break;
+					case "RICK":
+						var song = "We're no strangers to love You know the rules and so do I A full commitments what Im thinking of You wouldn't get this from any other guy I just wanna tell you how I'm feeling Gotta make you understand Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you";
+						
+						var x = 0;
+						var y = 0;
+						for(var i=0; i<song.length; i++) {
+							world[y*64+x] = song.charAt(i);
+							x++;
+							if(x>64) { y++; x=0; }
+						}
+						break;
+				}
+				
+				break;
+			
+		}
+	},
+	
+	requested: function(req, res) {
+		console.log('HTTP Request');
+	},
 
-								 console.log("Key Pressed ");
-								 console.log(data[0].dir); 			
-								 //find the player in the list
-								 for (var i in Players)
-								 {
-										 if (  client.id == Players[i].connect.id)
-										 {	
-												 switch(data[0].dir){
-														 case 'U': // W up
-															Players[i].moveUp(client);
-														 break;
-														 case 'R': // D right
-															Players[i].moveRight(client);
-														 break;
-														 case 'D': // S down
-															Players[i].moveDown(client);
-														 break;
-														 case 'L' : // A Left
-															Players[i].moveLeft(client);
-														 break;
-														default:
-															Players[i].moveLeft(client);
-					
-												 }
-										 }	
-								 }
-								 break;
-				 }
-		 },
-requested: function(req, res) {
-				   console.log('HTTP Request');
-		   },
-
-disconnected: function(client) {
-
-					  console.log('Disconnected:', client.id);
-					  // TODO remove from list
-			  }
+	disconnected: function(client) {
+		console.log('Disconnected:', client.id);
+	}
 });
 
 var protocol = {
 	type: {
 		INIT: 1,
 		COMMAND: 2,
-		WORLD : 3
-	} ,
+		WORLD: 3,
+		CONSOLE: 4
+	},
+	
 	cmd: {
 		MOVE: 1,
 		MINE: 2
 	},
+	
 	dir: {
 		NA: 0, // meaning doesn't apply
 		UP: 1,
@@ -219,57 +181,13 @@ var protocol = {
 	}
 };
 
-var commands = {
-		//?????? WHAT GOES HERE?????
-}
 var world_info = {
 	SCREEN_W: 32,
 	SCREEN_H: 32,
-	BLOCK_CHARS: "qwertyuiop[]\\asdfghkl;zxcvbnm,./1234567890-=!@$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?" //purposely doesn't include #, reserved for robots/players
 };
 
 var srv = new PieceServer();
 srv.start({
 	port: 4000,
-	logicRate: 100
+	logicRate: 1
 });
-/*
-       //////////////////////////////////////////////////////////////\
-      //////////////////////////////////////////////////////////////--\
-     //////////////////////////////////////////////////////////////----\
-    /////////////SETUP STATIC HTTP SERVER/////////////////////////------\
-   //////////////WARNING! VERY INSECURE!!////////////////////////________\
-  //////////////////////////////////////////////////////////////         /
- //////////////////////////////////////////////////////////////---------/
-//////////////////////////////////////////////////////////////---------/
-\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\--------/
- \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\------/
-  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\----/
-   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\--/
-    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/
-*/
-
-var http = require('http');
-var fs = require('fs');
- 
-http.createServer(function (request, response) {
- 
-    console.log('request starting...');
-	//console.log(request);
-     
-    fs.readFile('../client'+request.url, function(error, content) {
-        if (error) {
-			console.log(error)
-            response.writeHead(500);
-            response.end();
-        }
-        else {
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.end(content, 'utf-8');
-        }
-    });
-     
-}).listen(80);
- 
-console.log('Server running at http://127.0.0.1/');
-
